e3287c7537ae8719f91385d666d5c2533d9fdf2b
Siarhei Hanko <siarhei.hanko@thomsonreuters.com>
2019-10-03 17:45:14 +0300

    Multithreading task 6

--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,9 +24,41 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            ShowMenu();
 
             Console.ReadLine();
+        }
+
+        private static void ShowMenu()
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.WriteLine("\n******** MENU ********");
+                Console.WriteLine("a) - Continuation task should be executed regardless of the result of the parent task.");
+                Console.WriteLine("b) - Continuation task should be executed when the parent task finished without success.");
+                Console.WriteLine("c) - Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
+                Console.WriteLine("d) - Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
+                Console.WriteLine("ESC - Exit");
+                cki = Console.ReadKey(false);
+                Console.Clear();
+
+                switch (cki.KeyChar.ToString())
+                {
+                    case "a":
+                        TaskRunner.RunA();
+                        break;
+                    case "b":
+                        TaskRunner.RunB();
+                        break;
+                    case "c":
+                        TaskRunner.RunC();
+                        break;
+                    case "d":
+                        TaskRunner.RunD();
+                        break;
+                }
+            } while (cki.Key != ConsoleKey.Escape);
         }
     }
 }
